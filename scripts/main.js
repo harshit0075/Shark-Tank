f407b14871caad31e05d4c24bf3d651033d2646b
masaiharshit <harshit007.ara@gmail.com>
2023-08-09 23:13:28 +0530

    product

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+// --- do not touch  ↓↓↓↓↓↓↓↓↓↓↓↓ ----------
+const baseServerURL = `http://localhost:${
+  import.meta.env.REACT_APP_JSON_SERVER_PORT
+}`;
+// --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
+
+// ***** Constants / Variables ***** //
+const pitchURL = `${baseServerURL}/pitches`;
+let mainSection = document.getElementById("data-list-wrapper");
+
+// pitch
+let pitchTitleInput = document.getElementById("pitch-title");
+let pitchImageInput = document.getElementById("pitch-image");
+let pitchCategoryInput = document.getElementById("pitch-category");
+let pitchfounderInput = document.getElementById("pitch-founder");
+let pitchPriceInput = document.getElementById("pitch-price");
+let pitchCreateBtn = document.getElementById("add-pitch");
+
+// Update pitch
+let updatePitchIdInput = document.getElementById("update-pitch-id");
+let updatePitchTitleInput = document.getElementById("update-pitch-title");
+let updatePitchImageInput = document.getElementById("update-pitch-image");
+let updatePitchfounderInput = document.getElementById("update-pitch-founder");
+let updatePitchCategoryInput = document.getElementById("update-pitch-category");
+let updatePitchPriceInput = document.getElementById("update-pitch-price");
+let updatePitchBtn = document.getElementById("update-pitch");
+
+//Update price
+let updatePricePitchId = document.getElementById("update-price-pitch-id");
+let updatePricePitchPrice = document.getElementById("update-price-pitch-price");
+let updatePricePitchPriceButton = document.getElementById("update-price-pitch");
+
+//sort and filter
+let sortAtoZBtn = document.getElementById("sort-low-to-high");
+let sortZtoABtn = document.getElementById("sort-high-to-low");
+let filterFood = document.getElementById("filter-Food");
+let filterElectronics = document.getElementById("filter-Electronics");
+let filterPersonalCare = document.getElementById("filter-Personal-Care");
+
+//Search by title/founder
+
+let searchBySelect = document.getElementById("search-by-select");
+let searchByInput = document.getElementById("search-by-input");
+let searchByButton = document.getElementById("search-by-button");
